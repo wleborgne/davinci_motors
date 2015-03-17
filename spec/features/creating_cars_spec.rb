5bd43c672d948284d1cebe976db98e56c27117f8
Bill LeBorgne <bill@leborgne.name>
2015-03-17 14:15:53 -0600

    [#90545416] Add second test case
    Added test case to create and verify display of two cars.
    This could use some more refactoring to reduce duplication,
    but I'm not sure how at the moment.

--- spec/features/creating_cars_spec.rb
+++ spec/features/creating_cars_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 feature 'Creating Cars' do
   let(:car1) { { make: 'Ford', model: 'Mustang',
                  year: '1967', price: '2300' } }
+  let(:car2) { { make: 'Chevrolet', model: 'Corvette',
+                 year: '1957', price: '15000' } }
+
   scenario 'can create a car' do
     visit '/'
     
@@ -17,7 +20,41 @@ feature 'Creating Cars' do
     click_button 'Create Car'
     
     expect(page).to have_content('1967 Ford Mustang created')
-    # save_and_open_page
     expect(page).to have_content('$2,300.00')
+  end
+  
+  scenario 'can create two cars' do
+    # Create the first car
+    # This first bit is redundant; need to find how to
+    # abstract this process out of the test case
+    visit '/'
+
+    click_link 'New Car'
+
+    fill_in 'Make', with: car1[:make]
+    fill_in 'Model', with: car1[:model]
+    fill_in 'Year', with: car1[:year]
+    fill_in 'Price', with: car1[:price]
+
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('1967 Ford Mustang created')
+    expect(page).to have_content('$2,300.00')
+
+    # Create second car; just copying from previous steps
+    click_link 'New Car'
+
+    fill_in 'Make', with: car2[:make]
+    fill_in 'Model', with: car2[:model]
+    fill_in 'Year', with: car2[:year]
+    fill_in 'Price', with: car2[:price]
+
+
+    click_button 'Create Car'
+
+    expect(page).to have_content('1957 Chevrolet Corvette created')
+    expect(page).to have_content('$15,000.00')
+
   end
 end
