161ddffafc24f788929192b5a8a785ee5cd31c47
Bill LeBorgne <bill@leborgne.name>
2015-03-18 17:51:44 -0600

    [#90545416] Add extra specs for validations
    Added specs to create a car with no price and to add a price to
    a car with no price

--- spec/features/edit_car_spec.rb
+++ spec/features/edit_car_spec.rb
@@ -27,4 +27,15 @@ feature 'Edit cars' do
     # Verify new price appears on page
     expect(page).to have_content('$2,500.00')
   end
+  
+  it 'allows price to be added to car with no price' do
+    visit '/'
+    no_price = Hash.new().merge(car1)
+    no_price[:price] = nil
+    fill_create_form(no_price)
+    click_link 'Edit'
+    fill_in 'Price', with: '3000'
+    click_button 'Update Car'
+    expect(page).to have_content('$3,000.00')
+  end
 end
