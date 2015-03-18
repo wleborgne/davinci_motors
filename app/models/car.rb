e70ec206c272c42e21d2c45aaeb3f6d12386ef72
Bill LeBorgne <bill@leborgne.name>
2015-03-18 17:06:30 -0600

    [#90545416] Add validations to model
    Added validations to car model:
     - Requires make, model, numeric year
     - Does not require price, but requires numeric if present
    Updated spec to verify validations

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,6 @@
 class Car < ActiveRecord::Base
+  validates :make, presence: true
+  validates :model, presence: true
+  validates :year, presence: true, numericality: true
+  validates :price, allow_nil: true, numericality: true
 end
