8deb342cb9ee075605e80ebced4c767ce318acf9
Bill LeBorgne <bill@leborgne.name>
2015-03-29 16:20:12 -0600

    [#91320534] Refactor cars factory and spec
    Refactor cars factory to use FactoryGirl to generate make and model,
    and otherwise randomize year and price
    Refactor car_spec to use the new factory

--- spec/factories/cars.rb
+++ spec/factories/cars.rb
@@ -1,9 +1,13 @@
 FactoryGirl.define do
   factory :car do
-    make "MyString"
-    model "MyString"
-    year 1
-    price "9.99"
+    make { Faker::Company.name }
+    model { Faker::Team.creature }
+    year { rand(1940..2015)}
+    price { format('%.2f', rand(2000..15000)) }
+    # make "MyString"
+    # model "MyString"
+    # year 1
+    # price "9.99"
   end
 
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  let(:valid_car) { { make: 'Ford', model: 'Mustang',
-    year: '1967', price: '2300' } }
+  # let(:valid_car) { { make: 'Ford', model: 'Mustang',
+  #   year: '1967', price: '2300' } }
 
   it 'is expected to be a Car' do
-    car = Car.new(valid_car)
+    car = FactoryGirl.create(:car)
     expect(car).to be_a(Car)
   end
 
   context 'is valid when it' do
     it 'has all four attributes' do
-      car = Car.new(valid_car)
+      car = FactoryGirl.create(:car)
       expect(car.valid?).to be_truthy
     end
 
     it 'has no price' do
-      no_price_attributes = Hash.new().merge(valid_car)
+      no_price_attributes = FactoryGirl.attributes_for(:car)
       no_price_attributes[:price] = nil
       car = Car.new(no_price_attributes)
       expect(car.valid?).to be_truthy
@@ -25,35 +25,35 @@ RSpec.describe Car, type: :model do
 
   context 'is invalid when it' do
     it 'has no make' do
-      no_make = Hash.new().merge(valid_car)
+      no_make = FactoryGirl.attributes_for(:car)
       no_make[:make] = nil
       car = Car.new(no_make)
       expect(car.valid?).to be_falsey
     end
 
     it 'has no model' do
-      no_model = Hash.new().merge(valid_car)
+      no_model = FactoryGirl.attributes_for(:car)
       no_model[:model] = nil
       car = Car.new(no_model)
       expect(car.valid?).to be_falsey
     end
 
     it 'has no year' do
-      no_year = Hash.new().merge(valid_car)
+      no_year = FactoryGirl.attributes_for(:car)
       no_year[:year] = nil
       car = Car.new(no_year)
       expect(car.valid?).to be_falsey
     end
 
     it 'has a non-numeric year' do
-      bad_year = Hash.new().merge(valid_car)
+      bad_year = FactoryGirl.attributes_for(:car)
       bad_year[:year] = 'zero'
       car = Car.new(bad_year)
       expect(car.valid?).to be_falsey
     end
 
     it 'has a non-numeric price' do
-      bad_price = Hash.new().merge(valid_car)
+      bad_price = FactoryGirl.attributes_for(:car)
       bad_price[:price] = 'zero'
       car = Car.new(bad_price)
       expect(car.valid?).to be_falsey
